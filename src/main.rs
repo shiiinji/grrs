6083de7a2264121a06a55512c14fc01363325de7
Shinji Teramoto <shinji.toys@gmail.com>
2021-06-07 22:16:08 +0900

    add 1.6

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use anyhow::{Context, Result};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+  pattern: String,
+  #[structopt(parse(from_os_str))]
+  path: std::path::PathBuf,
+}
 
 fn main() -> Result<()> {
-  let path = "test.txt";
-  let content =
-    std::fs::read_to_string(path).with_context(|| format!("could not read file `{}`", path))?;
-  println!("file content: {}", content);
+  let args = Cli::from_args();
+  let content = std::fs::read_to_string(&args.path)
+    .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
+  grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
+
   Ok(())
 }
